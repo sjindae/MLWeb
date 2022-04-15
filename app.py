d07785d5df09cfbe7d9440961bb5bf5591e4a74e
sjindae <tnwjd9113@naver.com>
2022-04-15 14:17:15 +0900

    mnist 결과 페이지 완성

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask,render_template,request
-import os
+import os,pickle
+from PIL import Image
+import numpy as np
 
 app = Flask(__name__)
 
@@ -15,7 +17,14 @@ def mnist():
         f = request.files['mnistfile']
         path = os.path.dirname(__file__)+'/upload/'+f.filename
         f.save(path)
-        return "성공!!"
+        img = Image.open(path).convert("L")    # 그레인스케일로 바뀐다는디? 그게 모야>?
+        img = np.resize(img,(1,784))
+        # img = 255 - (img)
+        mpath = os.path.dirname(__file__)+'/model1.pickle'
+        with open(mpath,'rb') as f:
+            model = pickle.load(f)
+        pred = model.predict(img)
+        return render_template('mnistresult.html',data=pred) # "성공!!"+str(pred)
 
 
 if __name__ == '__main__':
